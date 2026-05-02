4cd65114ae89369e805edaf41a4b779aee513607
yodahe-dev <devyodahe@gmail.com>
2026-05-02 11:36:14 +0300

    .

--- models/Packaging.ts
+++ models/Packaging.ts
@@ -15,7 +15,8 @@ type PackagingCreationAttributes = Optional<
 export default (sequelize: Sequelize) => {
   class Packaging
     extends Model<PackagingAttributes, PackagingCreationAttributes>
-    implements PackagingAttributes {
+    implements PackagingAttributes
+  {
     public id!: string;
     public name!: string;
     public readonly createdAt!: Date;
@@ -31,7 +32,7 @@ export default (sequelize: Sequelize) => {
       },
 
       name: {
-        type: DataTypes.STRING,
+        type: DataTypes.STRING(50),
         allowNull: false,
         unique: true,
 
@@ -44,15 +45,23 @@ export default (sequelize: Sequelize) => {
             msg: "Packaging type too long",
           },
         },
+
         set(value: string) {
-          this.setDataValue("name", value.trim());
-        }
+          this.setDataValue("name", value.trim().toLowerCase());
+        },
       },
     },
     {
       sequelize,
       tableName: "packagings",
       timestamps: true,
+
+      indexes: [
+        {
+          unique: true,
+          fields: ["name"],
+        },
+      ],
     }
   );
 
